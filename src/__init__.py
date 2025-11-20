b975d91fa550c48999f4583a1a4a0d366108749f
Jonas <jonaseuler98@gmail.com>
2025-11-20 19:26:58 -0300

    setup app

---
+++ src/__init__.py
@@ -0,0 +1,7 @@
+# apiKey: "AIzaSyCwKydj84PcDpYnZ0PYDg3EuirF2MzlluI",
+#     authDomain: "linguatracker.firebaseapp.com",
+#     projectId: "linguatracker",
+#     storageBucket: "linguatracker.firebasestorage.app",
+#     messagingSenderId: "397086752066",
+#     appId: "1:397086752066:web:508ea22ab66393945e1cdb",
+#     measurementId: "G-Q345G2K4FP"
